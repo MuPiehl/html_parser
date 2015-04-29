068bc5a4690ef5ce09bfbafab4859eacc0381958
matthias.piehl <ma@wurst.de>
2015-04-29 18:56:44 +0200

    Example: url könnte geladen werden -- scheitert am Proxy

--- src/main/java/de_mpi/App.java
+++ src/main/java/de_mpi/App.java
@@ -1,13 +1,34 @@
 package de_mpi;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+//        URL url = new URL("http://xv-omc.hypoport.local/om-xv/index.html");
+        URL url = new URL("http://dilbert.com/"); // Intern führt dies zu 401 Unauthorized
+        // siehe https://wiki.hypoport.local/pages/viewpage.action?pageId=26051428
+
+        URLConnection con = url.openConnection();
+
+
+        Reader r = new InputStreamReader(con.getInputStream(), "UTF-8");
+        StringBuilder buf = new StringBuilder();
+        while (true) {
+            int ch = r.read();
+            if (ch < 0)
+                break;
+            buf.append((char) ch);
+        }
+        System.out.println(buf.toString());
+
     }
 }
